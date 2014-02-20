68613b6a6ead3d4619285e8d9173ac77036dcb77
Trent Petersen <tpetersen@engr-131-29.eastern.ewu.edu>
2014-02-20 14:50:00 -0800

    1st

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,17 @@ $(document).foundation();
 // Your Awesome Scripts!
 $(document).ready(function(){
 
-	console.log('Page Loaded. Lets Do this!');
+	// returns 0 to 'limit'
+	var limit = 2;
+	var numRand = Math.floor(Math.random()*limit);
+	var msg0 = 'hello!';
+	var msg1 = 'hola!';
+	var msg2 = 'bon dia!';
+
+	$('h1').text(msg+numRand);
+
+
+
+	console.log(numRand);
 
 }); 
